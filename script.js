3c92fb04d8d3c1518ad1a35cc253f45cca2ba941
isah6767 <144401149+isah6767@users.noreply.github.com>
2023-11-09 12:44:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function search(){
+var input = document.getElementById(`searchbar`)
+input = input.toLowerCase()
+let x = document.getElementsByClassName(`pesquisar`)
+}
+for(i = 0; i < x.length; i++){
+    if(!x)
+}
