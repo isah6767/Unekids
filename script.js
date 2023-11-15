9109cb0aa71e7e7849d074be0ef43cea244d5db4
isah6767 <144401149+isah6767@users.noreply.github.com>
2023-11-14 22:02:48 -0300

    comit local

--- script.js
+++
@@ -1,8 +0,0 @@
-function search(){
-var input = document.getElementById(`searchbar`)
-input = input.toLowerCase()
-let x = document.getElementsByClassName(`pesquisar`)
-}
-for(i = 0; i < x.length; i++){
-    if(!x)
-}
